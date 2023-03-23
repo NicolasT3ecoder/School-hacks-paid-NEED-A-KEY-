e82ffbb6e01f87f9732bc40c04002ac91aee0edb
Nicolas M <127147029+NicolasT3ecoder@users.noreply.github.com>
2023-03-23 18:15:53 -0400

    2.99 normal price!

---
+++ edpuzzle-Highlight-Answer.js
@@ -0,0 +1,13 @@
+javascript: //made by: NicolasT3ecoder | Paid script |
+'require'=("edpuzzle.com").toString(string, 5);
+SetValueOf(importScripts("arguments"));
+  Get:RunService("connection"('local, arguments'));
+Get:localService('colorType'('BLACK'));
+//this section highlights the answer
+SVGAElement(COLOR("BLACK"))
+ getSelection("importscripts").collapse = true;
+   innerHeight = (3); 
+SelectInnerColor = #color=000000 = true;
+  set(localElement('RunService')); 
+get:localService(RunService) = true;
+   (console.log("command")); ('require'("edpuzzle.com")); KeyScript.customElement("arguments") = true;
