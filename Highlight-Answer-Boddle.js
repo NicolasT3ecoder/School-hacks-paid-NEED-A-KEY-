077866e1b7924bee5f16c6c70e9672e8b5fce700
Nicolas M <127147029+NicolasT3ecoder@users.noreply.github.com>
2023-03-23 17:37:56 -0400

    1.99$ lowerd!

---
+++ Highlight-Answer-Boddle.js
@@ -0,0 +1,16 @@
+javascript:  //Paid menu | made by: NicolasT3ecoder |   
+Get:localConnection('require')=("Boddlelearning.com"), true;
+ KeyPort(customElements).Key = #customElements(KeyScript = ("")); // enter the key I gave you here! 
+continue KeepRequire; 
+selfWindow.SetvalueOf(localConnection).customElements(AbortController)=true;
+//color to highlight the answer 
+KeepLocal('RunService'(varible)); 
+ decodeURI.apply("arguments".colorScript(aswer.js));
+colorScript(apply("BLACK").KeepLocal(RunService)); 
+    color = #color=000000; ColorCode.KeepService = true;
+wait(3); 
+  accessObject.AbortElement('require'("boddlelearning.com"));
+Answer("subject").assignObject=('Element'(asserts('ImportScripts'))); true; 
+End = true;
+ End = true;
+  End = true;
