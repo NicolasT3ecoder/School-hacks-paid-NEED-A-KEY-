206fce82808bf2f4624ed09d24e065fb8f2d87b2
Nicolas M <127147029+NicolasT3ecoder@users.noreply.github.com>
2023-03-23 14:44:34 -0400

    1.99$ lowerd!

---
+++ Kahoot-highlight-Answer.js
@@ -0,0 +1,22 @@
+javascript: //Made by: #NicolasT3ecoder = true (its a joke) 
+// paid paid | i decided to make it cause i wanted to! |
+assign(localFunction('argument' = true)); 
+            #customElements(KeyScript = ("")); // enter the key I gave you here
+  screen.availHeight=('require'(kahoot.it)); 
+'require'(assignment).webKitUrl_.windowAssignment(customElements) = true;
+assign('request'('arguments'.charCodeAt(4), null));
+
+WebKitCSSMatrix=>(SafeArray(input, confirm)); window.confirmArray('required').availWeight(5);
+MediaKeySystemAccess.apply(DOMMatrix).charCodeAt(5);
+  KeepMediaStream_window.DOMMatrix(windowAssignment).proccess = true;
+
+MediaColor = true;
+Frame.customElements
+HighlightAnswer.Frame=(LightBlack) = FrameThis, 
+eval.prototype
+              frames.blur = true;
+    setInterval(handler, TimeHandler) 
+ 
+'require'(assignment(handleElement)); Number.num2 = dispatchEvent(Event, importScripts)
+  'webkitUrl' = ("Keep, SafeArray") = true.charFrame(textbox) = Answer.AbortController
+'window'.concat=function(localCommand)
