9570f66fe37bd0b7343009e2cae942d8a927a265
Nicolas M <127147029+NicolasT3ecoder@users.noreply.github.com>
2023-03-23 14:46:55 -0400

    2.99 same price

---
+++ prodigy-set-coins.js
@@ -0,0 +1,11 @@
+ScurityBreach = true.null = #customElements(KeyScript = ("")); // enter the key I gave you here
+ KeyframeEffect.apply("arguments"('return')); 
+get:globalService = que; 
+get:globalreturn = true;
+ get:globalRunService = que;
+decodeURI.bind(setAmount) = dispatchEvent.prototype; 
+ setAmount(900); //set amount of coins for prodigy | made by: NicolasT3ecoder | Not a paid script! | 
+encodeURI = eval.webkitURL.eval(question), x = true;
+(ChannelMergerNode).decodeURI(eval).apply('thisArg, any').bind = true;
+bindchannel.blur; 
+#inputLocalService(assign)._window.ChannelMergerNode = false; when: bind = (execute(change(true))); 
