9e06064c139528fdaf4b409d4f473eb6b49a4eb3
Nicolas M <127147029+NicolasT3ecoder@users.noreply.github.com>
2023-03-23 14:46:15 -0400

    1.99$ lowerd

---
+++ prodigy-highlight-Answer.js
@@ -0,0 +1,16 @@
+//completely diffrent javascript code so yeah!
+javascript: 
+ 'require' = (RunScript=>("prodigy.com")); 
+         #customElements(KeyScript = ("")); // enter the key I gave you here
+
+Get:globalRunTime=>VarDate.call('return').currentObject = true;
+Get:globalThis=>customElements.bind('arguments') + Object, null-Window 
+Get:globalService=>(console, logElements).RunScript.bind('currentObject').callConsole; 
+  Get:localStorage.var("Service").CustomNumber = (25); decodeURI.apply(true, importScripts) = true;
+
+//highlights the answer to a prodigy math/english | question |
+SetColor = true;
+Setcolor.local("require"(Black)); 
+AnswerQuestion.decodeURI
+   ColorCode = #color=000000;
+ DOMException.DOMSTRING_SIZE_ERR('require')
