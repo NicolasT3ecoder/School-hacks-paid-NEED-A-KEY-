91cae20565bfc1c96d79d2dd4e04f59e1915899b
Nicolas M <127147029+NicolasT3ecoder@users.noreply.github.com>
2023-03-23 14:43:17 -0400

    4.99$

---
+++ OwnClerver.js
@@ -0,0 +1,323 @@
+javascript: 
+//paid menu By: #NicolasT3ecoder custom made for nobody :( | clever hack to be the owner | might get banned in a couple of days or so |
+  // the clever script can make you owner of clever | what are the features? | the features include : changing people's account name's, deleting a clever/classroom, Auto log into account's, and you can, delete a clever account!
+//if you got this for free from someone there's a specific key that you need to run the script
+
+//key module custom key script | what to do? | 1. buy the script from ME/NicolasT3ecoder | 2. i will tell you the key | 3. #input the key where it says special KEY | and then have fun with the owner |
+
+Console.Custom = true.null.AbortKeySignal("") //special KEY goes there! it's 6-letters long BTW
+continue; CustomKey.signal = true.valueOf(KEY4); 
+continue; specialFIleKey.Get;globalKey.AccountControl = 'require'.includes(endsWith).assignLocalFunction_window
+continue;   customElements.define(script, KeyboardEvent).#inputSignal.var = 'require'
+  continue; #inputKey.customElements = 'Enter' + 'Enter', null.ControllAccount = specificKeyCode.customElement = true;
+continue; console.input = customElement.AbortKeySignal('null-purpose') = KeyVarible.A; 
+  continue; 'require' = #inputSignalEvent,script(InputEvent("KeySignal"(MessageAI) = ("if KEY is CORRECT then let command execute")));
+  continue; AutoSet(false) = false, AutoSet(true) = true; if ("key is correct"(correction = access)); 
+connect:
+if ("key is wrong"(correction = unaccess)); 
+
+var pop;
+'require' = ("clever.com")
+pop = new Audio('pop.mp3'); wait(5); 
+
+HTMLAllCollection.apply = ("arguments"); true
+'required' = ("clever.com"); true
+HTML_.window.apply = ("confirme");
+ Get:globalService = que;
+ Get:globalTribute = que;
+Get:GlobalRunTime = true
+('num1, num2, num3,') = MediaStream.apply("arguments"); true
+SupplyEvent.apply = ("GlobalRunTime").Text;(connect);; 
+ApplyEvent(ThisArg. num1) = (confirm).arguments("RunService")
+Get:localStorage('RunService'(connect)=true);; 
+   window.webkitURL.callService = concepts(request);
+(console.log = print_.varible(1));
+(console.log = print_.service(1));
+ (GlobalRunTime._window.HTMLService.any = toString());
+       Get:globalRunService = que;
+       Get:globalService = true;
+       Get:globalTribute = que;
+        Get:GlobalRunTime = ("confirm"(varible._window));
+    Get:Console.log = true; 
+ #input.window
+ #InputEvent.var
+ #inputLocalScript.MediaStream = que;
+ #inputLocalRunService.apply("vardate") 
+ #inputLocalRunTime.varibleAny(ThisArg) 
+(connect), localStorage = (selfWindow).Console 
+'require' = toString("clever.com") = TextTrackCueList = globalThis('serVice')
+                                            (connect);  Get:webkitURL = 'connection' 
+                                            (connectUrl); 'require' = toStringBar("clever.com"); execute("SetClipBoard")
+         (GlobalRunTime)=(connect);'RunService'-decodeURI._window.wait(0.5);
+         (connectUri) 
+          (globalService)=(connect);'RunService'-decodeURI._window.wait(1);
+        ('function')=(RunService);'require'-decodeURI.name_.window; var; 
+'required' = ("connect");
+ #outputLocalScript 
+ #OutputEvent.var 
+ #Output.MediaStream = true;
+ #outputLocalRunService.apply("arguments")
+ #outputLocalRunTime.vardateAny(ThisArg
+); 
+Outputs = true 
+'reconnection' = false.valueOf(0); 
+ReDue.idleRequest._window; 
+ var requestAccess_window=Get;globaltribute,getSelection_window.ApplyEvent=true;
+amount.access = console.log; controllerAccess.selfWindow=true
+Get:AccountControl = Set; valueOf(10) = true; TouchEvent.name = ("tostring"(
+)); true; 
+'contextEvent'(Get, RunService.any) = Get;selectionStyle
+'arguments'.matchAllService = Get, Selection.ApplyEvent; true
+   var selfWindow, selection=Get;globalThis,getComputedStyle_window.RunService(true
+   ); true 
+//connection check for varible.log's | VERY IMPORTANT for the SCRIPT!
+
+ReadableStream = show, ScreenX; if ('disconnection'); Get:getComputedStyle.bindElements = true
+#var ('confirm'), RunTime = localStorage.window(VarDate, Any); true
+#VarDate.console = true, SupplyEvent.HTMLService = ('nul1, nul2, nul3,')
+#varViewPort.connectURL.apply('argArray') = /1 + 1/, EvalError('nul1') = true
+#varible ('ExtrudeEvent') = 'RunService'-connection._check 
+#connectionCheck.window = EvalError; 'require' ("clever"), RTCPeerConnection.apply("arguments"); supplyEvent + supplyDemand, true
+Get:localConnection.apply('concept'); removeEventListener.caller = EventSource = from; supplyEvent = true
+SVGFEFuncRElement.prototype('connection').Get, RunService = supplyEvent._window; 
+('check'), localConnection; sessionStorage.clear = clearInterval.apply("clever.com") = true
+TextTrackCue = screenY; def(local);
+                        call(clearInterval);
+                         decodeURI.caller(length(3));
+        setInterval._window.EventSource = arguments.supplyEvent;'require'._window = endswitch 
+    setInterval(10); webkitURL; EvalError.console.log = MediaError.bind("supply"); Abortsignal, ('return'); true
+'Check' ("connection"), #confirm = true 
+#confirm ('local')._connection_window.Abortsignal = true 
+SharedWorker ('require')._window = #confirm = true
+'required' = ("connection").apply("Function");; true  
+#localConnection.apply('window'(Get, localStorage
+)); 
+'require'._window = 'confirm'; true.valueOf(arguments('nul2' = true
+));
+webkitURL.SharedWorker.TextTrackCue._window=(_connection_window
+); 
+("arguments").webkitURL.apply, ('require' = ("clever.com"
+));
+('RunService'(WebKitCSSMatrix.connect_.window = 'apply'
+)); 
+ (connection) = que; ('confirm') = (_connection_window)
+ 
+//this script help's the A.I bot put the script into the console to make you the owner
+
+congst(Get, localService).Abortsignal.globalRunService('return')=true.printLocal
+print = localCongst.ApplyEvent('require')
+print = localConst.EventSource('arguments'); true.Console 
+eval = EvalError; importScripts <signal>Get;localConnection = true.controllerAccess //for controll access 
+ LocalAI.ApplyEvent = ('subscription') (Function(true), dispatchEvent.MSG = ("contact A.I element") 
+('require'(LocalAIElement)), //it's an A.I bot element.
+
+(loadstring(toString(define.bindElements.Abortsignal.EvalError.Get,{LocalAI}.define=true.debuggerAbortElement.loadstring()))), 
+
+//A.I bot controllerAccess | controll's what the A.I bot does and its commands.
+
+RTCPeerConnection.apply("arguments")) + supplyDemand, true
+Get:localConnection.apply('concept'); removeEventListener.caller = EventSource = from; supplyEvent = true
+SVGFEFuncRElement.prototype('connection').Get, RunService = supplyEvent._window; 
+('check'), localConnection; sessionStorage.clear = clearInterval.apply("clever.com") = true
+TextTrackCue = screenY; def(local);
+                        call(clearInterval);
+                         decodeURI.caller(length(3));
+        setInterval._window.EventSource = arguments.supplyEvent;'require'._window = endswitch 
+    setInterval(10); webkitURL; EvalError.console.log = MediaError.bind("supply"); Abortsignal, ('return'); true
+'Check' ("connection"), #confirm = true 
+#confirm ('local')._connection_window.Abortsignal = true 
+SharedWorker ('require')._window = #confirm = true
+'required' = ("connection").apply("Function");; true  
+#localConnection.apply('window'(Get, localStorage
+)); 
+'require'._window = 'confirm'; true.valueOf(arguments('nul2' = true
+));
+webkitURL.SharedWorker.TextTrackCue._window=(_connection_window
+); 
+("arguments").webkitURL.apply, ('require' = ("clever.com"
+));
+('RunService'(WebKitCSSMatrix.connect_.window = 'apply'
+));
+ //commands for the A.I for getting you the owner
+ LocalAIBot.custom.Module.XPathEvaluator(eval,
+); connect: Function.apply(AbortController).ThisArg.any = true.valueOf(10);
+('local.Commands') = ('MouseCurSorMovements'), Movements; limit(100); 
+connect: 
+ (toString(SVGAElement.apply('arguments').KeepController.SAVE('element').Function.Module=true.valueOf(globalThis))), queueMicrotask 
+wait(3) = true
+ ('local.commands') = ('MouseButton1Click'), click; limit(50); 
+ connect:
+(toString(SVGAdefine.Module('require').KeepController.DATA("defineELement").Function.Movements=true.valueOf(globalMovments))); 
+connect:
+  ('local.commands') = (AutoTyperKeys.js), Movements; limit(5,000); 
+wait(2);
+   (toString(SVGAnimateMotionElement.Module('AbortController').return_.controller.AUTO(SAVE.Function.Movements)=true.valueof(globalControllMovements))); 
+connect: 
+   //AutoTyping | for A.I bot | Auto typing module for AI bot |
+connect:
+   (toString(SVGAnimateMotionElement.Module('AbortController').return_.controller.AUTO(SAVE.Function.Movements)=true.valueof(globalControllMovements))); 
+("requireSignal") = true.FunctionModule("Message").return_Function = true; 
+('require').AbortController = true.change; valueof(fraction) /1 + 1/ 
+  MessageChannel.apply("Function"), true 
+ ('MessageA.I') = true.valueOf(), void
+CharacterData.bind(btoa)+(A.I), true.valueOf(Boolean), 
+   postMessage.arguments("server").Function.ListCommands, true 
+ Message.custom.Module = A; 
+Message.custom = true.AbortController(removeEventListener), true; 
+Message("customURL") = true.Message = ("aibot.com") = true.wrightMessage.AUTO = true;
+wait(3); then: 
+wright_back = true.return_valueOf(false).Module.return = Message = (console.log, print, true)("free")
+ (); define.Element
+ script.Element.debug = true
+debugger_.window.ELement<DataTransfer> true 
+ requestIdleCallback.apply("debug.js") = TextTrackCue.bind("script"); 
+Account, Owner.Account = true
+ ElementInternals.apply("owner").define
+wait(7), true 
+ execute.command = true;  
+ 
+const camelfy = str => {
+  // return overlay for A.I bot 
+  Wait(2) = true
+  if (str.indexOf('-') === -1) return str;
+
+  return str.split('-').reduce((acc, item, idx) => {A.I = idleRequest.define.AccountControl.AbortController = true; 
+    return idx === 0
+      ? acc + item
+      : acc + item.charAt(0).toUpperCase(fraction) + item.substring(1);
+  }, '');
+};
+ //same properties
+ Equip-properties.AUTO, true.valueOf("ACCOUNT")
+ 'require' = true
+ 'require',window.webkitURL = ("clever.com")
+ ('RunServiceTime').SharedWorker = true
+  if (HTMLOf(charAt())); true 
+'arguments' = true.Enable
+'arguments'.charCodeAt(2) 
+'arguments'.includes = (toString(Acc.Items) = 'require'); 
+"RunService" = (RunTime(0.5)); 
+("clever.import").importScripts = true
+<IDBCursor>= true 
+(GlobalRunTime = true.charCodeAt(2));
+(GlobalRunTime) = true.Acc + items; toString(charAt(2)); 
+//A.I achievments
+(GlobalAchievments) = true.valueOf(2); selfWindow.Abortsignal = false
+'GlobalAchievments'.SharedWorker = 'RequireTime'.endsWith(GlobalValue(4));
+'require'=(GlobalAchievments), #Outputs = connect;(GlobalValueOf(4));
+'RequiredWebsite' = true.AccountControl.Module = selfWindow, #IDBCursorWithValue(5); 
+#InputEvent = ("Messager.com/search/A.I_Bot").charAt(postMessage("hey A.I make sure to type in that code ok." = (module.Message))); true
+#GlobalAchievments = show; true
+#GlobalRunTime = wait(3); true.CharacterData.Abortsignal.KeepController.DataTransfer = true
+#output = true.selfWindow(connect) = true
+  //what to type | for A.I bot |
+connect:
+   (toString(SVGAnimateMotionElement.Module('AbortController').return_.controller.AUTO(SAVE.Function.Movements)=true.valueof(globalControllMovements))); 
+("requireSignal") = true.FunctionModule("Message").return_Function = true; 
+ // admin console 
+('require').AbortController = true.change; valueof(fraction) /1 + 1/ 
+  MessageChannel.apply("Function"), true 
+ ('MessageA.I') = true.valueOf(), void
+CharacterData.bind(btoa)+(A.I), true.valueOf(Boolean), 
+   postMessage.arguments("server").Function.ListCommands, true 
+ Message.custom.Module = A; 
+Message.custom = true.AbortController(removeEventListener), true; 
+Message("customURL") = true.Message = ("aibot.com") = true.wrightMessage.AUTO = true;
+wait(3); then: 
+wright_back = true.return_valueOf(false).Module.return = Message = (console.log, print, true)("free")
+ (); define.Element
+ script.Element.debug = true
+debugger_.window.ELement<DataTransfer> true 
+ requestIdleCallback.apply("debug.js") = TextTrackCue.bind("script"); 
+Account, Owner.Account = true
+ ElementInternals.apply("owner").define
+wait(7), true 
+ execute.command = true; 
+//AI.Brain here! And storage
+Local:globalService.AUTO('require').Abortsignal; true.valueOf(Boolean) //bot brain | tells the bot the messages \
+Local:Geolocation.apply('arguments').AUTO //bot brain | tells the bot what to wright
+  Get:LocalAI.ApplyEvent('Actions.Motions') = true.valueOf(Animation.toString()); //make's the bots decisions to move
+   #EnableInput.Abortsignal("controlActions"); Auto.toString(Symbol); true //controls action | check's connection
+   #EnableOutput.AbortController('return'("LocalActions")).ApplyEvent.service("SetLimit.Controls"(Decision)(SVGSymbolElement.A)); // make's the controlls then limits the action
+#SetLimit.controllerAccess = true.charCodeAt(2); true.apply(WindowService) = limit.access, null // tells the bot to limit the access above
+#SetSupply = true.Window_.SVGAdefine("Element".apply = TransformStream.bindElements=(TextTrackCue.true = null)); // it is the 2nd half of the brain | if you remove it the whole script will not work! |
+//Local storage is here | saves the code to the AI storage so u can do it again!
+SetLocallAction = true.AbortController.parent; LocalStorage.SAVE.ElementInternals; null.true
+KeepLocalStorage.Module = null.StoreLocalData("Storage.DATA").OuterControll=(connect); var 
+KeepLocalData, Module = VarDate.apply("StoreLocalStorage"("Inercontroll")=true.null); 
+      localStorage.getItem=("storage").setGB = true.valueOf(GigaByte.null);
+    SetLocallAction = SetAmount(500('GigaBytes') = null(script)._windowService);
+  LocalGigaBytes.RunService("RunConnection").callService(console).log = localConnection; LocalGigaBytes = SetAmount(500) //sets the gigabytes to any value | cannont actually store Localstorage | only stores GigaBytes. |
+LocalGigaBytes = LocalStorage.Get;(GlobalRunService('RunService.connection' = localData.null));
+localStorage.setItem("LocalStorage", "Storage");
+history.back(); //history for the bot | to get the right selections For? | clever.com
+history.go(-1); TransScript.File.module = 'return'='require'.charAt("clever.com")
+AddSearchProvider("clever.com").FileLister = true.null // connects to the | history | code to #input the search history to the TransScript.
+//SetAmount script | special made by #NicolasT3ecoder 
+("SetCLipBoard") = true.null; script(#input =  //special SetAmount script | For Gigabytes and for the movments.animations
+DOMMatrix.apply('concept') = null
+.call(apply(NaN)),
+SVGAnimatedBoolean) + JSON; true.que 
+('custom').SupplyEvent.window('supplyEvent'); 
+superEvent.DOMMatrix.window_.JSON = que; 
+Wait(3); (Window.caller = moveTo(JSON)); 
+  ("customEvent").charAt(pos, 1,).concat*webkitURL*window_("supply");
+'require'.confirm = VBArray.#bindDecodeURI
+'require' = true.null
+  'arguments'-Window(webkitURL(Function)); 
+(superEvent(selfWindow)); Number, 4;
+'require'-("SetClipBoard"(GigaBytes.Animations)); true
+      'subtract' = #BiquadFilterNode.Interface("");
+  requestIdleCallback.arguments("apply").Interface; true
+ 'Idle'.Handle('requestIdleCallBack') = interface.null 
+           5; + 5; 
+                   ('console').matchAll(RegExp); 
+                ("debugger").indexOf(searchString());
+            console.log = parseInt.apply("arguments") = clearInterval.bind.caller("CallBack.Auto"); RegExp.true
+SetAmount(500); GigaBytes = true.null
+ Console.log = SetAmount.Animations = true.SetAmount(superEvent);
+// logging into the console | for entering the command in the console | to make you the owner of | clever.com |
+console.log(execute("command"));
+console.log("Loading code in!"); wait(3);
+ console.log("Almost there!"); wait(2);
+ SetString(Console.log.execute("command")); true
+git_name = (clever.Owner_script
+); 
+Save_element("Get:LocalCommand" = setItem("console.log, execute"
+)); 
+assign(RegExp).Error, 'RunConsole'=Function.congst<init>=decodeURI, null
+wait(4), 
+console.log = ("code succesfully loaded in the console!")
+wait(1);
+       #input.logs = true.null, (requestAccess_window).A;
+wait(1);
+   #input, Interface.AbortController(then, wait(1));
+ #inputInterface = true.null = console.log(init(TextDecoder(apply("Auto.Data")))); 
+ webkitURL.call('callback')=VarDate.arguments(IDBCursorWithValue=(4));
+var assignLocalFunction_window; webkitURL.valueOf('callback'(4));
+Get:localService = DOMMatrix.apply("arguments") + 'callback', 'return'.RunStyle
+Get:localFunction = DOMException.NAMESPACE_ERR("name") = caches.return
+  Get:webkitURL.createObjectURL=(""(property, Screen)); 
+init.console.log = (SVGAElement), ("Function").indexOf("name") = true
+ if (console.log("timelog"(3), ("insert"))); 
+   init.console.RunStyle().DOMException = confirm.arguments
+  apply('Git, callBack').VarDate.apply = true.num1, 
+init.ControllAccount(assign)=apply(connect,).Get;globalThis.module("inforum").DOMException = true.num2,  
+when, localStorage = fullAmount(importScripts).ControllAccount = true
+   when, ("executed"(wait(3))); true.after=("importScripts") = null.TransScript
+connect: 
+valueOf(3.5); ('importScripts') = true.valueOf(Boolean).import = (NameValue() = (PasswordValue())); 
+connect:
+when: NameValue(); false = change; To (execute).wait(2), true = ('NameValue').null-window.var 
+connect:
+when: PasswordValue(); true = change; To (execute).wait(2.5), true = ("AutoEnter"), 'Enter' = ("MSGAI") = 'ENTER'
+connect: 
+("MSGAI")=("ai_bots.com") = 'MSG'; (grantAccess = true.valueOf(NamedNodeMap.apply(PasswordValue(grantAccess)))); true // this is the whole code | if you delete this it will not do anything | this script make's you the the cleverOwner |
+ module ("local") = {
+  var (LocalActions) 
+ }; 
+  //clever hack finished | for becoming the owner | may be banned by: clever ban script | made by: #NicolasT3eCoder = true | paid menu by nobody :( |
+  End = true;
+   End = que; ('confirm') = (_connection_window)
+    End = true
+     End = true;
