2a58fca8002c6ffabe09216c7023f934a9b918da
Nicolas M <127147029+NicolasT3ecoder@users.noreply.github.com>
2023-03-23 19:27:24 -0400

    UpdateCommonScripts

---
+++ Main.lua/Auto-update.lua
@@ -0,0 +1,14 @@
+--AutoUpdate
+ --version 1.1
+--Date: 3/23/23
+
+('LocalUpdate'(localStorage) = break);
+  Update=>(Auto, log) = Get:localUpdate; 
+
+print(LocalUpdate);
+print(LocalValue(Update));
+decimal(assign)=3.0; Set:AutoUpdate 
+
+Update ::= [var; exp,]
+Set:RegExp ::=> [Auto; local,]
+  Get:local-Range ::=> [local; decimal]()
