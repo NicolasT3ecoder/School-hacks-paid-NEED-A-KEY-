1540403123c0ed81a51b67bddc7f742a1d7bcf28
Nicolas M <127147029+NicolasT3ecoder@users.noreply.github.com>
2023-03-23 14:48:08 -0400

    2.99 same price

---
+++ Blooket-set-coin-Hack.js
@@ -0,0 +1,11 @@
+javascript:  //made by | NicolasT3ecoder! |
+'require' = ("Blooket.com") = #customElements(KeyScript = ("")); // enter the key I gave you here!
+   setInterval(handler, timehandler) = true.null;
+ 'arguments'.charCodeAt(importScripts).toExponential = selfWindow = true;
+encodeURI.apply("arguments").apply.local = setTimeout(5); 
+check: localFunction.import-File, (SetAmount('localFunction'(webkitURL))); 
+  RegExp.bind-Caller(assign('class')); location.hostname = ('console, insert') = (localStorage).getItem=("coin, Set My Coins!"), null;
+DOMMatrix.fromMatrix("bind, caller"(assignment).setTimeout(2)); 
+  (local(importScripts).DOMMatrix(setAmount("setAmount" = true))); 
+SaveLocalElement.apply(setAmount(900)); //set the amount of coins you want | made by: NicolasT3ecoder | 
+// same properties
