281ab015047d5b028071dfeb9c7695979982c1db
Nicolas M <127147029+NicolasT3ecoder@users.noreply.github.com>
2023-03-23 14:45:34 -0400

    1.99$ lowerd!

---
+++ Blooket-highlight-answer.js
@@ -0,0 +1,23 @@
+//same script with a little adjustments 
+javascript: //Made by: #NicolasT3ecoder = true.joking.OK 
+assign(localFunction('argument' = true)); 
+            #customElements(KeyScript = ("")); // enter the key I gave you here
+  screen.availHeight=('require'(Blooket.com)); 
+'require'(assignment).webKitUrl_.windowAssignment(customElements) = true;
+assign('request'('arguments'.charCodeAt(4), null));
+
+WebKitCSSMatrix=>(SafeArray(input, confirm)); window.confirmArray('assign._window').availWeight(5);
+MediaKeySystemAccess.apply(ValidityState).charCodeAt(5); ValidityState.prototype = toString(format)
+  KeepMediaStream_window.DOMMatrix(windowAssignment).proccess = true; (KeepMediaStream).connect, ("localArguments")=true, 
+
+MediaColor = true;
+Frame.customElements
+HighlightAnswer.Frame=(LightBlack) = FrameThis, 
+eval.toString(StaticRange).length = 5;
+              frames.blur = true;
+    setInterval(handler, localHandler) 
+ 
+'require'(assignment(handleElement)); Number.num2 = dispatchEvent(Event, importScripts)
+  'webkitUrl' = ("Keep, SafeArray") = true.charFrame(textbox) = Answer.AbortController
+'window'.concat=function(localCommand)  
+ //same properties
