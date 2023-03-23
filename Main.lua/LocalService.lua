dd9c4f0e23963724ba0fa48f3d1b6b5d31c1c233
Nicolas M <127147029+NicolasT3ecoder@users.noreply.github.com>
2023-03-23 19:11:34 -0400

    MAIN(localService)

---
+++ Main.lua/LocalService.lua
@@ -0,0 +1,13 @@
+--MainService 
+ --Version 1.1
+--Date: 3/23/23 
+
+(LocalService) = (function()); 
+  <div (local('SetService'(validate)));
+local-value('return')=>(KeepService(update = return.update
+)); 
+   ('RunService'( return Service or return Service = Get:LocalService
+   ));
+UpdateVersion(Feature(repeat = "localService") and MainService = nil 
+   <div = KeepNil('until'(5)));
+Local(Service("SetService"(repeat)));
