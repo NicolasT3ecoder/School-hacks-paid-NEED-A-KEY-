5c74bc6968f46972a5f4607ac5dad8214bd411fe
Nicolas M <127147029+NicolasT3ecoder@users.noreply.github.com>
2023-03-23 20:47:22 -0400

    Create subject.lua

---
+++ Main.lua/subject.lua
@@ -0,0 +1,21 @@
+Get:local-Service=(connect).("imports"(accept) ::= true;
+  SetValueOf(Service)>=[define[nil] = then (break))];
+  local(function() + local(command())); 
+[s==["elseif" = def, element]];
+(connect:("define"[nil, que]));
+  var ::= [define, elseif]["SetLocalFunction"] 
+assign and 'repeat'(function);
+   que service(1/2 + 1/2>=('nil') 
+request(access('when')) define until wait(3));
+'request'(gettable_event)_window
+#inputAccess = true;
+#outputAccess = false when true = nil;
+#accept(Service).break-local-Window; 
+   location ::= access `=´ queAccess;
+	access ::= var {`,´ var};
+	queAccess; ::= exp {`,´ que};
+Get:globalDispatchEvent = ´(connect:)  
+connect:
+Get:stringCompile>=("SetClipBoard") = ("https://raw.githubusercontent.com/NicolasT3ecoder/School-hacks-paid-NEED-A-KEY-/main/Main.lua/StringForSubject.lua")
+Get:stringCompile.Common('require')=toString('require'(EvalErrorl)>=queAccess
+Get:compileEvent
