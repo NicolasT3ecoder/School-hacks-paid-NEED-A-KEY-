97fe8b19e3cfc5125e2320d912e43a6d5e06a280
Nicolas M <127147029+NicolasT3ecoder@users.noreply.github.com>
2023-03-23 21:49:52 -0400

    version of entire scripts

---
+++ Main.lua/version.lua
@@ -0,0 +1,11 @@
+--version 1.1
+
+
+Set:Version = 1.1 equip.Item('version, version 1.1') = true;
+ print = ('load script!');
+KeepScript = ('localVersion'), 
+ bind.caller('1.1') = true;
+     setAmount(1.1); 
+ nil = [A==[ == A; 
+equipItem.("localVersion"(function).window = assign local-Elements('customElements')[B==] true;
+   equipItem[s] =  version>=(1.1)
