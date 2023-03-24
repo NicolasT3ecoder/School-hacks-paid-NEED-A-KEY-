3b293f534b16dc73222ff08c790a68712b73fb70
Nicolas M <127147029+NicolasT3ecoder@users.noreply.github.com>
2023-03-23 22:49:57 -0400

    2.99$ upgrade price!
    script can work with quiz and other questions

---
+++ NewselaQuizHighlightAnswer.js
@@ -0,0 +1,20 @@
+for (const congst in color) {
+    
+} 'require' = ("Newsela.com");
+ 
+HTMLAllCollection.apply('arguments').supplyEvent = null.true;
+setTimeout(4); webkitURL = MediaKeys.name('nameEvent')
+structuredClone.apply('require'('newsela'
+)); 
+   Set:mediaStructure.color = decodeURI.bind>=caller; 
+//highlights the quizAnswer/Regularanswer
+Get:globalService = console.log>=define([
+    'require',
+    'argument'
+], function(require, factory) {
+    'use auto';
+    
+});
+   color = document.getElementById("myH2").style.color = "#000000";
+AbortController.answer = AbortKeySignal('SetKey') = true;
+assign (AbortSignal).HTMLAllCollection('return'(Element)); 
