0f71e96bc7aabe1a68d4b4bbcdd486880aedf2b2
Nicolas M <127147029+NicolasT3ecoder@users.noreply.github.com>
2023-03-23 20:34:46 -0400

    string for subject

---
+++ Main.lua/StringForSubject.lua
@@ -0,0 +1,21 @@
+Get:local-Service=(connect).("imports"(accept) ::= true;
+  SetValueOf(Service)>=[define[nil] = then (break))];
+  local(function() + local(command())); 
+[==["elseif" = def, element]];
+(connect:("define"[nil, que]));
+  var ::= [define, elseif]["SetLocalFunction"] 
+assign and 'repeat'(function);
+   que service(1/2 + 1/2>=('nil') 
+request(access('when')) define until wait(3));
+'request'(gettable_event)_window
+#inputAccess = true;
+#outputAccess = false when true = nil;
+#accept(Service).break-local-Window; 
+   location ::= access `=´ queAccess;
+	access ::= var {`,´ var};
+	queAccess; ::= exp {`,´ que};
+Get:globalDispatchEvent = ´(connect:)   q
+    
+ 
+
+        
