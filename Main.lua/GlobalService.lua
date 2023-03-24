e90caf1063d5571a43be640ae6bb3c0c39c1fda6
Nicolas M <127147029+NicolasT3ecoder@users.noreply.github.com>
2023-03-23 21:09:34 -0400

    Create GlobalService.lua

---
+++ Main.lua/GlobalService.lua
@@ -0,0 +1,8 @@
+[congst[s==[set x; = true.nil
+
+Get:GlobalTribute('customElement'('arguments') = setTrue, then setValueOf('globalTribute'));
+   Get:globalRunService(connect:("service").Box = setValueOf(customElement)>=´´changeElement = true;
+continue at Get:SetService.customElement = 1/2 + 1/2 = ´window_.active after Wait(3) = true;
+  Const + const 
+           Get:globalRunService´ = nil, 
+      Get:globalService>=service.SelectValue ::= [windowOfService] = true;
