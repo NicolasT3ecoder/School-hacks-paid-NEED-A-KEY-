54be33ab39ef1acedac88d05e357f5e52464d3d7
Nicolas M <127147029+NicolasT3ecoder@users.noreply.github.com>
2023-03-23 21:34:21 -0400

    enables the localfunction on customElement

---
+++ Main.lua/localFunction.lua
@@ -0,0 +1,11 @@
+ --enables the localfunction customELement
+local-function = true;
+ assign ('request'("arguments") = nil) = customELement;
+local = var 'localRequest' [local, requestScript[importScript = nil]];
+  Get:localfunction = true then set valueof(function());
+(function().apply(valueof(localfunction) function = [2 x 3('function')>=true then ('execute') after wait(3)))];
+  set ('localfunction') = accept after 'Limit'>=requestScript('apply') = true; 
+   SetLocalAmount>=true; 
+ 'request'(localAccessAmount) = *describeELement* 
+  //same properties
+    
