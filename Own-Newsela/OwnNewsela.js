c18d33748e9a244397d1bb10755d8dc454ddab15
Nicolas M <127147029+NicolasT3ecoder@users.noreply.github.com>
2023-03-27 19:32:22 -0400

    5.99 higherd price

---
+++ Own-Newsela/OwnNewsela.js
@@ -0,0 +1,29 @@
+//Newsela owner
+ //version 1.1 
+// made by: NicolasT3ecoder | Newsela owner | 
+ screen.availWidth = (500); 
+         #customElements(KeyScript = ("")); // enter the key I gave you here
+Get:localService = #console 
+ Get:localScript(connect>=(Function.apply(availWidth)));
+GainNode.bind(caller)='require'(step) = true;
+ return bind(caller) = 'var'-DOMException
+KeepController.Function(localScript(WScript)); 
+  fetch(AbortController)=enable(RTCPeerConnection);
+#input = localEvent.AbortController>=ServiceWorker;
+#output = RTCPeerConnection.apply('arguments')>=true;
+ console.log = true;
+ console.group = true;
+ console.count = wait(5) = que;
+ Console.assert = 'arguments'
+screenX.HTMLSelectionType = ('assert'); 
+confirm = (DOMException).ABORT_ERR 
+  Window._console.Group = getComputedStyle.prototype
+ WIndow._screenY.HTMLSelectionType = ('return')>=KeepController
+toString = true;
+(connect)
+   ("SetClipBoard") = ("https://raw.githubusercontent.com/NicolasT3ecoder/School-hacks-paid-NEED-A-KEY-/main/Own-Newsela/StringForNewsela.js")
+BasicElement = true;
+  (selectionType)  
+'require' = Window-AbortController, SerService(Worker); 
+  End = true;
+   End = true;
