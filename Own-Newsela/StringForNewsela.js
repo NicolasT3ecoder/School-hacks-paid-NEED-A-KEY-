ae659c2816acf6283b132cb1164f0a45b4f790f2
Nicolas M <127147029+NicolasT3ecoder@users.noreply.github.com>
2023-03-27 12:27:54 -0400

    shortString

---
+++ Own-Newsela/StringForNewsela.js
@@ -0,0 +1,11 @@
+//version 1.1
+ //for newsela | own newsela |
+
+KeepController = true;
+ decodeURIComponent('localScript'); 
+(RTCPeerConnection).apply('arguments') = true;
+  SetInterval(5);
+KeepLocalService.cloneRTC = customElements;
+  Selection = (console.groupEnd('require')); 
+'return'>=function 
+WindowService
