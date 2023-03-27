5a14b515b5985e3488821c42dc97e8e1db222c27
Nicolas M <127147029+NicolasT3ecoder@users.noreply.github.com>
2023-03-27 19:40:29 -0400

    properties that define Element

---
+++ Own-Newsela/IN or OUT-Event.js
@@ -0,0 +1,11 @@
+(DataTransferItemList) = true;
+  ("title") = FrontList
+SVGAElement.apply('arguments') = customElements.upgrade 
+ Prototype = true;
+#InputEvent>=(ListEvent(RTCPeerConnection));
+#OutputEVent<=('return'('arguments')=RTCPeerConnection = true;
+  KeepController = true; 
+End = true;
+ End = true;
+  End = true;
+   End = true;
